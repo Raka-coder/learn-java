d463f5637e3bbd09ada6e2464b5d1ac979b561b9
Raka <incudelta@gmail.com>
2025-09-11 23:18:15 +0700

    refactor: menambahkan gir

--- SepedaGunung.java
+++ SepedaGunung.java
@@ -3,6 +3,6 @@ public class SepedaGunung extends Sepeda {
     // Method untuk mengatur nilai pedal
     void setGir(int jumlah){
         super.setGir(jumlah);
-        gir=2*jumlah;
+        gir=2*getGir();
     }
 }
