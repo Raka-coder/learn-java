dfc02272f1315a8f55c8b916640ece4f6e2a5794
Raka <incudelta@gmail.com>
2025-09-30 19:35:07 +0700

    Merge branch 'main' of https://github.com/Raka-coder/learn-java

--- KendaraanBeraksi2.java
+++ KendaraanBeraksi2.java
@@ -1,7 +1,7 @@
 class KendaraanBeraksi2 {
 	public static void main(String[] args){
 		Mobil mobil1 = new Mobil();
-		mobil1.merek = "Toyota";
+		mobil1.merk = "Toyota";
 		mobil1.jumlahPintu = 4;
 		mobil1.jalan();
 		
