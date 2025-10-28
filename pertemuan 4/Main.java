f5a725f96c96a5e83a7d481931782351dc44acb4
Raka <incudelta@gmail.com>
2025-10-28 11:37:37 +0700

    add: input - build in packages

---
+++ pertemuan 4/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inputan = new Scanner(System.in);
+
+        System.out.print("Masukkan Username: ");
+        String username = inputan.nextLine();
+
+        System.out.println(username);
+    }
+}
