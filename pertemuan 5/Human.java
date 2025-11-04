764584780c7aedca0bfdb5fa32d664f071aa65d1
Raka <incudelta@gmail.com>
2025-11-04 11:40:46 +0700

    add: getter * setter

---
+++ pertemuan 5/Human.java
@@ -0,0 +1,16 @@
+public class Human {
+    private String name;
+    private int age;
+
+    public void Greeting() {
+        System.out.println("Hello, Good Morning!");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ pertemuan 5/Main2.java
@@ -0,0 +1,16 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Human h1 = new Human();
+        // h1.name = "Ginanjar";
+        // h1.age = 20;
+
+        h1.setName("Ginanjar");
+        String name = h1.getName();
+        // h1.setAge(20);
+        
+        System.out.println(name);
+        System.out.println(h1.getName());
+        // System.out.println(h1.);
+
+    }
+}
